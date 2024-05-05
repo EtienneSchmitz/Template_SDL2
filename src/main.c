f42daa15b6f6225b5fde859e85d6a1afd49cfd98
Etienne Schmitz <contact@etienne-schmitz.com>
2024-05-05 18:42:07 +0200

    feat : add soldier move

--- src/main.c
+++ src/main.c
@@ -6,15 +6,48 @@
 
 #include <stdbool.h>
 
+#define VELOCITY_X 3.0
+#define VELOCITY_Y 3.0
+
+void update_screen_velocity(SDL_Event event, float* xvel, float* yvel) {
+    switch (event.type) {
+    case SDL_KEYDOWN:
+        switch (event.key.keysym.sym) {
+        case SDLK_UP:
+            *yvel = -1.0;
+            break;
+        case SDLK_DOWN:
+            *yvel = 1.0;
+            break;
+        case SDLK_RIGHT:
+            *xvel = 1.0;
+            break;
+        case SDLK_LEFT:
+            *xvel = -1.0;
+            break;
+        }
+        break;
+    case SDL_KEYUP:
+        switch (event.key.keysym.sym) {
+        case SDLK_UP:
+        case SDLK_DOWN:
+            *yvel = 0.0;
+            break;
+        case SDLK_RIGHT:
+        case SDLK_LEFT:
+            *xvel = 0.0;
+            break;
+        }
+        break;
+    }
+}
+
+
 int main(int argc, char* argv[]) {
     SDL_Window* window = NULL;
     SDL_Renderer* renderer = NULL;
     SDL_Texture* imageTexture = NULL;
-    TTF_Font* font = NULL;
-    SDL_Surface* textSurface = NULL;
-    SDL_Texture* textTexture = NULL;
-    Mix_Music* music = NULL;
-
+    
     // Initialisation de SDL
     if (SDL_Init(SDL_INIT_VIDEO | SDL_INIT_AUDIO) < 0) {
         printf("SDL could not initialize! SDL_Error: %s\n", SDL_GetError());
@@ -27,77 +60,64 @@ int main(int argc, char* argv[]) {
         printf("SDL_image could not initialize! SDL_image Error: %s\n", IMG_GetError());
         return -1;
     }
+  
 
-    // Initialisation de SDL_ttf
-    if (TTF_Init() == -1) {
-        printf("SDL_ttf could not initialize! SDL_ttf Error: %s\n", TTF_GetError());
-        return -1;
-    }
-
-    // Initialisation de SDL_mixer
-    if (Mix_OpenAudio(44100, MIX_DEFAULT_FORMAT, 2, 2048) < 0) {
-        printf("SDL_mixer could not initialize! SDL_mixer Error: %s\n", Mix_GetError());
-        return -1;
-    }
-
-    window = SDL_CreateWindow("SDL Extensions Test", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 640, 480, SDL_WINDOW_SHOWN);
+    window = SDL_CreateWindow("SDL Rectangle TEST", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 640, 480, SDL_WINDOW_SHOWN);
     renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
 
     // Chargement de l'image
-    imageTexture = IMG_LoadTexture(renderer, "./assets/image/texture.jpg");
+    imageTexture = IMG_LoadTexture(renderer, "./assets/image/soldier/soldier.png");
     if (!imageTexture) {
         printf("Unable to load image! SDL_image Error: %s\n", IMG_GetError());
+        SDL_DestroyRenderer(renderer);
+        SDL_DestroyWindow(window);
+        printf("Erreur lors du chargement de l'image: %s\n", IMG_GetError());
+        return 1;
     }
-
-    // Chargement de la police et affichage du texte
-    font = TTF_OpenFont("./assets/font/Ancient Medium.ttf", 28);
-    SDL_Color textColor = { 255, 255, 255 }; // Blanc
-    textSurface = TTF_RenderText_Solid(font, "Test SDL_ttf!", textColor);
-    textTexture = SDL_CreateTextureFromSurface(renderer, textSurface);
-    SDL_FreeSurface(textSurface);
-
-    // Chargement et lecture de la musique
-    music = Mix_LoadMUS("./assets/audio/animal.wav");
-    if (!music) {
-        printf("Failed to load music! SDL_mixer Error: %s\n", Mix_GetError());
-    }
-    else {
-        Mix_PlayMusic(music, -1);
-    }
-
+   
     // Boucle principale
     SDL_Event e;
     bool quit = false;
+    // Création du point
+    SDL_Rect rect;
+    rect.x = 0;  // Position horizontale de l'image sur l'écran
+    rect.y = 0;  // Position verticale de l'image sur l'écran
+    rect.w = 32;  // Largeur de l'image
+    rect.h = 32;  // Hauteur de l'image
+    float x = 0.0, y = 0.0, xvel = 0.0, yvel = 0.0;
+
     while (!quit) {
+
         while (SDL_PollEvent(&e) != 0) {
             if (e.type == SDL_QUIT) {
                 quit = true;
             }
+            else {
+                update_screen_velocity(e, &xvel, &yvel);
+            }
         }
 
+
+        SDL_SetRenderDrawColor(renderer, 0xFF, 0xFF, 0xFF, 0xFF);
         SDL_RenderClear(renderer);
-        SDL_RenderCopy(renderer, imageTexture, NULL, NULL); // Affiche l'image
 
-        // Affiche le texte
-        int textWidth = 0;
-        int textHeight = 0;
-        TTF_SizeText(font, "Test SDL_ttf!", &textWidth, &textHeight);
-        SDL_Rect renderQuad = { 10, 10, textWidth, textHeight };
-        SDL_RenderCopy(renderer, textTexture, NULL, &renderQuad);
+        
+		x += VELOCITY_X * xvel;
+		y += VELOCITY_Y * yvel;
+        rect.x = (int) x;
+        rect.y = (int) y;
+        SDL_RenderCopy(renderer, imageTexture, NULL, &rect);
 
-        SDL_RenderPresent(renderer);
+        SDL_RenderPresent(renderer);       
+        SDL_Delay(16);
     }
 
     // Nettoyage
     SDL_DestroyTexture(imageTexture);
-    SDL_DestroyTexture(textTexture);
-    TTF_CloseFont(font);
-    Mix_FreeMusic(music);
+    IMG_Quit();
+    
     SDL_DestroyRenderer(renderer);
     SDL_DestroyWindow(window);
-    IMG_Quit();
-    TTF_Quit();
-    Mix_Quit();
     SDL_Quit();
 
     return 0;
